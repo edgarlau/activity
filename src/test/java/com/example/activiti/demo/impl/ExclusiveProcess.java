ceba0ab21f8e219e538f37b42a1b313a8983b221
innjia <13671682486@sina.cn>
2018-04-04 19:06:38 +0800

    3th commit code

--- src/test/java/com/example/activiti/demo/impl/ExclusiveProcess.java
+++ src/test/java/com/example/activiti/demo/impl/ExclusiveProcess.java
@@ -4,9 +4,11 @@ import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.List;
 
 public class ExclusiveProcess {
 
@@ -37,5 +39,20 @@ public class ExclusiveProcess {
         String piKey = "exclusiveProcess";
         ProcessInstance processInstance = processEngine.getRuntimeService().startProcessInstanceByKey(piKey);
         System.out.println("PID: "+processInstance.getId());
+    }
+
+    @Test
+    public void queryPersonTaskList(){
+        String userId = "张三";
+        List<Task> list = processEngine.getTaskService().createTaskQuery().taskAssignee(userId).list();
+
+        if (list!=null && list.size()>0){
+            for (Task task :list
+                 ) {
+                System.out.println("id: "+task.getId()+"\nname: "+task.getName()+"\nAssignee: "+task.getAssignee()+"\nCreateTime: "+task.getCreateTime()+"\nexecution"+task.getExecutionId());
+                System.out.println("============================");
+
+            }
+        }
     }
 }
